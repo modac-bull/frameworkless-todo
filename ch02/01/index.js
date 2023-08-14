6d4e44656618131c426b7789280fd5c72577af53
leejihoon <ljhljh211@naver.com>
2023-08-14 22:37:11 +0900

    docs: ch02-01 add code description

--- ch02/01/index.js
+++ ch02/01/index.js
@@ -1,3 +1,4 @@
+/* 모듈 임포트 */
 import getTodos from "./getTodos.js";
 import view from "./view.js";
 
@@ -8,6 +9,11 @@ const state = {
 
 const main = document.querySelector(".todoapp");
 
+/* 
+ 브라우저의 다음 리페인트가 발생하기 직전에 특정 코드를 실행하고 싶을 때 사용
+ 모든 DOM 조작이나 애니메이션은 이 DOM API를 기반으로 해야 한다.
+ 이 API는 메인 스레드를 차단하지 않으며 다음 리페인트가 이벤트 루프에서 스케줄링 되기 직전에 실행된다.
+*/
 window.requestAnimationFrame(() => {
   const newMain = view(main, state);
   main.replaceWith(newMain);
--- ch02/01/view.js
+++ ch02/01/view.js
@@ -29,6 +29,12 @@ const getTodoCount = (todos) => {
 export default (targetElement, state) => {
   const { currentFilter, todos } = state;
 
+  /* 대상 요소의 복제본을 만든다.
+  - 원본 DOM 요소에 직접 변경을 가하지 않고 변경된 새 요소를 반환하기 위함
+  - 직접적인 DOM 조작이 반복 => 리페인트/리프로우를 수행해야 할 수 있음
+  - 솔루션 : 오프스크린(virtual, detached)에서 요소를 조작한 후 그 결과를 한 번에 DOM 에 적용하는 방법
+
+  */
   const element = targetElement.cloneNode(true);
 
   const list = element.querySelector(".todo-list");
@@ -48,3 +54,13 @@ export default (targetElement, state) => {
 
   return element;
 };
+
+/* /////////  코드 리뷰  ////////////
+
+1. 하나의 거대한 함수
+=> 여러 DOM 요소를 조작하는 함수가 단 하나뿐, 복잡해질 수 있음
+
+2. 동일한 작업을 수행하는 여러 방법
+=> 문자열을 통해 리스트 항목을 생성.
+
+*/ /////////////////////////////////
